ba23dc21040d335a3e8ccbff1881433c887f9844
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2025-03-08 21:55:26 +0300

    Question iterator with progress

--- mult.py
+++ mult.py
@@ -1,15 +1,31 @@
 from random import shuffle
 
-tasks = []
-for a in range(1, 11):
-    for b in range(a, 11):
-        tasks.append((a, b))
-shuffle(tasks)
+
+class Questions:
+    def __init__(self):
+        self.questions = []
+        for a in range(1, 11):
+            for b in range(a, 11):
+                self.questions.append((a, b))
+        shuffle(self.questions)
+
+    def __iter__(self):
+        self.i = -1
+        return self
+
+    def __next__(self):
+        self.i += 1
+        l = len(self.questions)
+        if self.i == l:
+            raise StopIteration
+        progress = int(((self.i + 1) / l) * 100)
+        return (*self.questions[self.i], progress)
+
 
 solved = 0
 mistakes = 0
 
-for a, b in tasks:
+for a, b, progress in Questions():
     expected = a * b
     actual = None
     while True:
@@ -18,7 +34,6 @@ for a, b in tasks:
             actual = int(input().strip())
             if expected == actual:
                 solved += 1
-                progress = int((solved / len(tasks)) * 100)
                 print(f"✅ прогресс {progress}%, ошибок: {mistakes}")
                 break
             else:
